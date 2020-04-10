5cb540be18282ded73fc57993d86589c3d72eb43
Howard Zhang <how613@hotmail.com>
2020-04-10 12:58:26 -0400

    more changes

--- index.js
+++ index.js
@@ -20,11 +20,31 @@ app.get('/', (req, res)=>{
 app.get('/api/users', db.getUsers);
 app.post('/api/loginUser', db.loginUser);
 app.post('/api/searchBooksByName', db.searchBooksByName);
+app.post('/api/searchBooksByGenre', db.searchBooksByGenre);
+app.post('/api/searchBooksByPublisher', db.searchBooksByPublisher);
+app.post('/api/searchBooksByPriceLessThan', db.searchBooksByPriceLessThan);
+app.post('/api/searchBooksByPriceGreaterThan', db.searchBooksByPriceGreaterThan);
 app.post('/api/searchForBookPublisher', db.searchForBookPublisher);
 app.post('/api/createNewOrder', db.createNewOrder);
 app.post('/api/createBookOrder', db.createBookOrder);
 app.post('/api/changeStockCount', db.changeStockCount);
 app.post('/api/searchForMyOrders', db.searchForMyOrders);
+app.get('/api/getExpenditures', db.getExpenditures);
+app.get('/api/getSalesPerGenre', db.getSalesPerGenre);
+app.get('/api/getSalesPerAuthor', db.getSalesPerAuthor);
+app.post('/api/addBook', db.addBook);
+app.post('/api/removeBook', db.removeBook);
+app.post('/api/searchForAuthor', db.searchForAuthor);
+app.post('/api/createUser', db.createUser);
+app.post('/api/searchForAuthorByName', db.searchForAuthorByName);
+app.post('/api/addAuthor', db.addAuthor);
+app.post('/api/addWrites', db.addWrites);
+app.get('/api/addWritesNewAuthor', db.addWritesNewAuthor);
+app.post('/api/searchForPublisherByName', db.searchForPublisherByName);
+app.post('/api/addPublisher', db.addPublisher);
+app.post('/api/addBookWithNewPublisher', db.addBookWithNewPublisher);
+app.post('/api/searchBooksByAuthor', db.searchBooksByAuthor);
+app.post('/api/getDetailsOfOrder', db.getDetailsOfOrder);
 
 app.listen(port, ()=>{
     console.log(`App running on port ${port}.`);
--- public/javascripts/queries.js
+++ public/javascripts/queries.js
@@ -42,6 +42,56 @@ const searchBooksByName = (request, response) =>{
     });
 }
 
+const searchBooksByGenre = (request, response) => {
+    const {query} = request.body;
+    pool.query('select * from book where upper(book.genre) like upper(concat(\'%\', \'' + query + '\', \'%\'))', (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const searchBooksByPublisher = (request, response) => {
+    const {query} = request.body;
+    pool.query('select * from book where publisher_id = (select id from publisher where upper(name) like upper(concat(\'%\',\''+ query + '\', \'%\')))', (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const searchBooksByPriceLessThan = (request, response) => {
+    const {query} = request.body;
+    pool.query('select * from book where price <= $1', [query], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const searchBooksByPriceGreaterThan = (request, response) => {
+    const {query} = request.body;
+    pool.query('select * from book where price >= $1', [query], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const searchBooksByAuthor = (request, response) => {
+    const {query} = request.body;
+    pool.query('select * from book where book.id in (select book.id from book inner join writes on writes.book_id=book.id inner join author on writes.author_id=author.id where author_id=(select id from author where upper(name) like upper(concat(\'%\',\''+ query + '\', \'%\'))))', (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
 const searchForBookPublisher = (request, response) => {
     const {publisherID} = request.body
     pool.query('select publisher.name from publisher where publisher.id = $1', [publisherID], (error, results) =>{
@@ -92,6 +142,157 @@ const searchForMyOrders = (request, response) => {
     })
 }
 
+const getExpenditures = (request, response) =>{
+    pool.query('select * from book_order inner join book on book.id=book_order.book_id inner join orders on book_order.order_id = orders.id where is_restock = true', (error, results)=>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getSalesPerGenre = (request, response) =>{
+    let strSQL = "select genre, sum(quantity*(book.price - (book.price*book.pub_percentage))) from book inner join book_order on book.id = book_order.book_id where is_restock = false group by genre";
+    pool.query(strSQL, (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getSalesPerAuthor = (request, response) =>{
+    let strSQL = "select author.name, author.alt_names, sum((book.price - (book.price*book.pub_percentage))*book_order.quantity) from book inner join writes on book.id = writes.book_id inner join author on author.id = writes.author_id inner join book_order on book.id = book_order.book_id group by author.name, author.alt_names";
+    pool.query(strSQL, (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+// adds a book with a given publisher name (and the publisher exists)
+const addBook = (request, response) => {
+    const {bookTitle, bookPublisher, bookISBN, bookGenre, bookNumPages, bookDatePublished, bookPrice, bookStockCount, bookPubPercentage, bookPubPrice} = request.body;
+    pool.query("insert into book values((select max(id)+1 from book), (select id from publisher where name=$2), $1, $3, $4, $5, $6, $7, $8, $9, $10)", [bookTitle, bookPublisher, bookISBN, bookGenre, bookNumPages, bookDatePublished, bookPrice, bookStockCount, bookPubPercentage, bookPubPrice], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const addBookWithNewPublisher = (request, response) =>{
+    const {bookTitle, bookISBN, bookGenre, bookNumPages, bookDatePublished, bookPrice, bookStockCount, bookPubPercentage, bookPubPrice} = request.body;
+    pool.query("insert into book values((select max(id)+1 from book), (select max(id) from publisher), $1, $2, $3, $4, $5, $6, $7, $8, $9)", [bookTitle, bookISBN, bookGenre, bookNumPages, bookDatePublished, bookPrice, bookStockCount, bookPubPercentage, bookPubPrice], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+
+}
+
+const removeBook = (request, response) => {
+    const {bookTitle} = request.body;
+    pool.query("delete from book where title = $1", [bookTitle], (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const searchForAuthor = (request, response) => {
+    const {bookTitle} = request.body;
+    pool.query("select author.name from book inner join writes on book.id = writes.book_id inner join author on writes.author_id = author.id where book.title = $1", [bookTitle], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const searchForPublisherByName = (request, response) => {
+    const {bookPublisher} = request.body;
+    pool.query("select * from publisher where name = $1", [bookPublisher], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const createUser = (request, response) =>{
+    const { registerName, registerEmail, registerPassword, registerBillingBuildingNum, registerBillingStreetName, registerBillingPostalCode, registerBillingCity, registerBillingProvince, registerBillingCountry, registerCardType, registerCardNumber, registerShippingBuildingNum, registerShippingStreetName, registerShippingPostalCode, registerShippingCity, registerShippingProvince, registerShippingCountry} = request.body;
+    console.log(request.body);
+    pool.query("insert into users values((select max(id)+1 from users), $1::text, $2, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, false, $3)", [registerName, registerEmail, registerPassword, registerBillingBuildingNum, registerBillingStreetName, registerBillingPostalCode, registerBillingCity, registerBillingProvince, registerBillingCountry, registerCardType, registerCardNumber, registerShippingBuildingNum, registerShippingStreetName, registerShippingPostalCode, registerShippingCity, registerShippingProvince, registerShippingCountry], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const searchForAuthorByName = (request, response) => {
+    const {authorName} = request.body;
+    pool.query("select * from author where name = $1 or $1=ANY(alt_names)", [authorName], (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const addAuthor = (request, response) =>{
+    const {authorName, birthday, altNames} = request.body;
+    pool.query("insert into author values((select max(id)+1 from author), $1, $3, $2)", [authorName, birthday, altNames], (error, results)=>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const addWrites = (request, response) =>{
+    const {authorID} = request.body;
+    pool.query("insert into writes values($1, (select max(id) from book))", [authorID], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const addWritesNewAuthor = (request, response) => {
+    pool.query("insert into writes values((select max(id) from author), (select max(id) from book))", (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const addPublisher = (request, response) =>{
+    const {publisherName, publisherBuildingNum, publisherStreetName, publisherCity, publisherProvince, publisherCountry, publisherPostalCode, publisherEmail, publisherBankAccountNum, publisherBankName} = request.body;
+    pool.query("insert into publisher values((select max(id)+1 from publisher), $1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [publisherName, publisherBuildingNum, publisherStreetName, publisherCity, publisherProvince, publisherCountry, publisherPostalCode, publisherEmail, publisherBankAccountNum, publisherBankName], (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getDetailsOfOrder = (request, response) =>{
+    const {id} = request.body;
+    pool.query("select (select title from book where book.id=book_order.book_id), (select date from orders where orders.id= $1), order_id, quantity, is_restock from book_order where book_order.order_id = $1", [id], (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
 module.exports = {
     getUsers,
     loginUser,
@@ -101,6 +302,26 @@ module.exports = {
     createBookOrder,
     changeStockCount,
     searchForMyOrders,
+    getExpenditures,
+    getSalesPerGenre,
+    getSalesPerAuthor,
+    addBook,
+    addBookWithNewPublisher,
+    removeBook,
+    searchForAuthor,
+    searchBooksByGenre,
+    searchBooksByPublisher,
+    searchBooksByAuthor,
+    searchBooksByPriceLessThan,
+    searchBooksByPriceGreaterThan,
+    createUser,
+    searchForAuthorByName,
+    addAuthor,
+    addWrites,
+    addWritesNewAuthor,
+    searchForPublisherByName,
+    addPublisher,
+    getDetailsOfOrder,
 }
 
 /*
